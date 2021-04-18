e122b365a4efd8fc5433ff7c588b1ec38abc1e33
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-04-18 14:19:51 -0500

    general mode script

--- ScriptDBMySQl/Script.sql
+++ ScriptDBMySQl/Script.sql
@@ -1,7 +1,9 @@
-CREATE DATABASE IF NOT EXISTS CIDENET;
-USE CIDENET;
+-- Script to create a database to be used by the front-end and back-end.
 
-CREATE TABLE IF NOT EXISTS Employees(
+CREATE DATABASE IF NOT EXISTS DB_NAME;
+USE DB_NAME;
+
+CREATE TABLE IF NOT EXISTS TABLE_NAME(
           ID int AUTO_INCREMENT,
    	  PrimerNombre varchar(20) not null,
      	  PrimerApellido varchar(20) not null,
